d00663a401dfd9450d70be0e8dd3d006e0f76421
Donbosco Kim <leonbosco2@gmail.com>
2024-02-18 13:34:14 -0600

    add a doctor model

---
+++ Model/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nupi_Clinic.Model
+{
+    internal class Doctor
+    {
+        public int DoctorID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Specialty { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
