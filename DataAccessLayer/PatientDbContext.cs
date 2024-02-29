670d71272b47527fb2ed1f9d9f42d71cbf2b38a1
Donbosco Kim <leonbosco2@gmail.com>
2024-02-29 10:19:36 -0600

    add Appointments class

--- DataAccessLayer/PatientDbContext.cs
+++ DataAccessLayer/PatientDbContext.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Nupi_Clinic.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Nupi_Clinic.DataAccessLayer
 {
---
+++ Model/Appointments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nupi_Clinic.Model
+{
+    public class Appointments
+    {
+        public int AppointmentID { get; set; }
+        public int DoctorID { get; set; }
+        public int PatientID { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        
+        public virtual Doctors? Doctors { get; set; }
+        public virtual Patients? Patients { get; set; }
+    }
+}
